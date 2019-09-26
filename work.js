7ee612002eda0bd40e6043d3578f1a7c2a16bafc
Henry Yeung <henryyeung@theorigo.com>
2019-09-26 09:58:40 +0800

    added conditional junction & display / hidden function

--- work.js
+++ work.js
@@ -8,6 +8,27 @@ var ph = new Vue(
       width: null,
       height: null,
       isx: null
+    },
+    computed: {
+      junction: function() {
+        var temp = '';
+        if ( this.url == 'via.placeholder.com' ) {
+          temp = 'x';
+        } else if ( this.url == 'image-p17.dev.theorigo.com' ) {
+          temp = 'c'
+        } else {
+          temp = '/'
+        }
+        return temp;
+      }
+    },
+    methods: {
+      display: function(i) {
+        return 'https://' + this.url + '/' + (this.width * i || "") + this.junction + (this.height * i || "" )
+      },
+      hidden: function(i) {
+        return this.display(i) + (this.suffix ? (i == 1 ? "" : " " + i + "x") : "")
+      }
     }
   }
 )
